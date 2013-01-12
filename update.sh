e09215045a126ebcce6ded91d8f5d8a9659e4cc1
Artem Sapegin <artem@sapegin.ru>
2013-01-12 20:57:54 +0400

    2.0.6.

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
-mkdir src
+mkdir -p src
 curl -o src/social-likes.css https://raw.github.com/sapegin/social-likes/master/social-likes.css
 curl -o src/social-likes.js https://raw.github.com/sapegin/social-likes/master/src/social-likes.js
 curl -o src/social-likes.min.js https://raw.github.com/sapegin/social-likes/master/social-likes.min.js
