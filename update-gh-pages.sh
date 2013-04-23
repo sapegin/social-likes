80ea0f61493da0f0b753a3b60eb75351a026bb44
Artem Sapegin <artem@sapegin.ru>
2013-04-23 11:31:00 +0400

    gh-pages updater script.

---
+++ update-gh-pages.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Updating Social Likes on gh-pages…"
+
+cp social-likes.css ../social-likes_gh-pages/src/
+cp social-likes.min.js ../social-likes_gh-pages/src/
+cp src/social-likes.js ../social-likes_gh-pages/src/
+pushd ../social-likes_gh-pages/
+git commit -m "Update Social Likes." src
+git push
+popd
