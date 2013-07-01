d8916a7ff74f07ac87903ecfbdef7c4268656bdb
Artem Sapegin <artem@sapegin.ru>
2013-07-01 22:46:03 +0400

    Update update script.

--- update.sh
+++ update.sh
@@ -2,3 +2,4 @@ mkdir -p src
 curl -o src/social-likes.css https://raw.github.com/sapegin/social-likes/master/social-likes.css
 curl -o src/social-likes.js https://raw.github.com/sapegin/social-likes/master/src/social-likes.js
 curl -o src/social-likes.min.js https://raw.github.com/sapegin/social-likes/master/social-likes.min.js
+git commit -m "New version." src
