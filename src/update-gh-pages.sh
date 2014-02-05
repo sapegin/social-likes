3af2d06c62b1a8e2dd3150faaf8e3d498d6d725a
Artem Sapegin <artem@sapegin.ru>
2014-02-05 18:30:08 +0400

    Add version number to gh-pages.

--- src/update-gh-pages.sh
+++ src/update-gh-pages.sh
@@ -7,8 +7,10 @@ cp ../social-likes_flat.css ../../social-likes_gh-pages/src/
 cp ../social-likes_classic.css ../../social-likes_gh-pages/src/
 cp ../social-likes_birman.css ../../social-likes_gh-pages/src/
 cp ../social-likes.min.js ../../social-likes_gh-pages/src/
+cp ../bower.json ../../social-likes_gh-pages/src/
 cp social-likes.js ../../social-likes_gh-pages/src/
 pushd ../../social-likes_gh-pages/
-git commit -m "Update Social Likes." src
+grunt replace:version
+git commit -m "Update Social Likes." src index.html ru/index.html
 git push
 popd
