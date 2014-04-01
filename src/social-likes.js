5ed3e956e22f63b92be50d60404d9afc76bfd9c1
Artem Sapegin <artem@sapegin.ru>
2014-04-01 13:11:21 +0400

    Always use HTTPS for Twitter (#58).

--- src/social-likes.js
+++ src/social-likes.js
@@ -42,11 +42,11 @@ var services = {
 		popupHeight: 500
 	},
 	twitter: {
-		counterUrl: protocol + '//urls.api.twitter.com/1/urls/count.json?url={url}&callback=?',
+		counterUrl: 'https://cdn.api.twitter.com/1/urls/count.json?url={url}&callback=?',
 		convertNumber: function(data) {
 			return data.count;
 		},
-		popupUrl: protocol + '//twitter.com/intent/tweet?url={url}&text={title}',
+		popupUrl: 'https://twitter.com/intent/tweet?url={url}&text={title}',
 		popupWidth: 600,
 		popupHeight: 450,
 		click: function() {
