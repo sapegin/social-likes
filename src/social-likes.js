38f6a12190a82e0251d7d94be328f1bb611c4eb3
Artem Sapegin <artem@sapegin.ru>
2015-11-21 12:59:59 +0100

    Update Social Likes.

--- src/social-likes.js
+++ src/social-likes.js
@@ -45,10 +45,6 @@
 			popupHeight: 500
 		},
 		twitter: {
-			counterUrl: 'https://cdn.api.twitter.com/1/urls/count.json?url={url}&callback=?',
-			convertNumber: function(data) {
-				return data.count;
-			},
 			popupUrl: 'https://twitter.com/intent/tweet?url={url}&text={title}',
 			popupWidth: 600,
 			popupHeight: 450,
@@ -338,7 +334,9 @@
 			}
 		},
 		updateCounter: function(e, service, number) {
-			if (number) {
+			number = number || 0;
+
+			if (number || this.options.zeroes) {
 				this.number += number;
 				if (this.single) {
 					this.getCounterElem().text(this.number);
