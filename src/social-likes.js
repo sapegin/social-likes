e7411b2ad2d0e95a899865bd292ea69fa31a7551
Mikhail Shevtsov <mikhail.shevtsov@gmail.com>
2016-12-02 00:45:21 +0300

    Update social-likes.js

--- src/social-likes.js
+++ src/social-likes.js
@@ -535,8 +535,8 @@
 		},
 
 		openPopup: function(url, params) {
-			var dualScreenLeft = window.screenLeft != undefined ? window.screenLeft : screen.left;
-			var dualScreenTop = window.screenTop != undefined ? window.screenTop : screen.top;
+			var dualScreenLeft = window.screenLeft !== undefined ? window.screenLeft : screen.left;
+			var dualScreenTop = window.screenTop !== undefined ? window.screenTop : screen.top;
 			var width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
 			var height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
 
